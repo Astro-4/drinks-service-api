3dc6c0d06d012e7d22ff23fddaa6e753ca1ad247
Tefo-K <tefokeabetswe4@gmail.com>
2023-08-19 23:37:20 +0100

    feat: added a new test for adding a chocolate mocha and updated the package.json file for npm start to be directed to server.ts specifically.

--- src/controllers/coffee_controller.test.ts
+++ src/controllers/coffee_controller.test.ts
@@ -8,3 +8,14 @@ test("GET /coffee should return correct object", async () => {
     name: "Latte",
   });
 });
+
+test("GET /coffee should return correct object", async () => {
+  const res = await request(app)
+    .get("/coffee")
+    .query({ coffeeName: "Chocolate Mocha" });
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: "Coffee",
+    name: "Chocolate Mocha",
+  });
+});
